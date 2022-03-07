40f89108ffb123fed51e113b3e6147d148384764
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-03-07 11:46:08 +0100

    added buttons
    remove the buttons on the table and make the table pass state to the buttons, so the buttons know when to show up and ehat to target

--- src/renderer/components/Routes/Invoices.jsx
+++ src/renderer/components/Routes/Invoices.jsx
@@ -3,10 +3,71 @@ import Table from '../Table/Table'
 import { Plusbutton } from '../misc/Plusbutton'
 import Modal  from '../Modal/Modal';
 import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const fs = require('fs');
 
+function Sidebuttons({type, onClick}) {
+  const hover = {
+    scale: 1.1,
+    y: [0, -10, 8, -8,0],
+  };
+  const onclick = {
+    scale: 0.9,
+  };
+  const fadein = {
+    hidden: {
+      x: "-100vh",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      opacity: 1,
+      transition: {
+        duration: 1,
+        type: "spring",
+        damping: 20,
+        stiffness: 200,
+      },
+    },
+    exit: {
+      
+      x: "-100vh",
+      opacity: 0,
+      transition: { duration: 0.5 }
+    },
+  };
+    return (
+    
+    <>
+        <motion.button
+            variants={fadein}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            whileHover={hover}
+            whileTap={onclick}
+            onClick="" className="side-button button1" onClick={onClick}><i class="fa-solid fa-file-invoice"></i></motion.button>
+        <motion.button
+            variants={fadein}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            whileHover={hover}
+            whileTap={onclick}
+            onClick="" className="side-button button2" onClick={onClick}><i class="fa-solid fa-pencil"></i></motion.button>
+        <motion.button
+            variants={fadein}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            whileHover={hover}
+            whileTap={onclick}
+            onClick="" className="side-button button3" onClick={onClick}><i class="fa-solid fa-trash-can"></i></motion.button>
+    </>
 
+    );
+}
 
 
 
@@ -45,6 +106,7 @@ function Invoices() {
   <>
       <div className='invoices-container'>
       <Plusbutton click={() => (modalOpen ? close() : open())}/>
+      <Sidebuttons click={() => (modalOpen ? close() : open())}/>
       <AnimatePresence>
       {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
       </AnimatePresence>
